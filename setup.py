4ccb49165edc8fc8a9a0221736bf7cc0749da8a4
Smaranjit Ghose <46641503+smaranjitghose@users.noreply.github.com>
2020-07-20 01:19:13 +0530

    Added Keywords in Setup File

--- setup.py
+++ setup.py
@@ -8,6 +8,9 @@ PACKAGE_NAME = 'PyConfidentiality'
 AUTHOR = 'Smaranjit Ghose,Anush Bhatia'
 AUTHOR_EMAIL = 'smaranjitghose@protonmail.com,anushbhatia1234@gmail.com'
 URL = 'https://github.com/you/pyconfidentiality'
+KEYWORDS = "rsa email-sender python cryptography smtp"
+
+
 
 LICENSE = 'MIT License'
 DESCRIPTION = 'Conceal your emails with Python'
@@ -24,7 +27,28 @@ setup(name=PACKAGE_NAME,
       author=AUTHOR,
       license=LICENSE,
       author_email=AUTHOR_EMAIL,
+      keywords = KEYWORDS,
       url=URL,
       install_requires=INSTALL_REQUIRES,
       packages=find_packages()
       )
+
+   classifiers = [
+       "Development Status :: 5 - Production/Stable",
+       "Topic :: Software Development :: Build Tools",
+       "License :: OSI Approved :: MIT License",
+       "Environment :: Console",
+       "Operating System :: OS Independent",
+       "Intended Audience :: Science/Research",
+       "Intended Audience :: Developers",
+       "Intended Audience :: Financial and Insurance Industry",
+       "Intended Audience :: Healthcare Industry",
+       "Topic :: Scientific/Engineering",
+       "Framework :: IPython",
+       "Programming Language :: Python :: 3",
+       "Programming Language :: Python :: 3.6",
+       "Programming Language :: Python :: 3.7",
+       "Programming Language :: Python :: 3.8",
+       "Programming Language :: Python :: 3.9",
+   ],
+   
